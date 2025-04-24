841938b65efec1ed2c2ec509a979c586b46e02e6
Bekir Inal <bekirinal5415@gmail.com>
2025-04-24 16:30:50 +0200

    Blokken sneller laten vallen

--- Opdracht C/dodge-the-blocks/features/blocks.py
+++ Opdracht C/dodge-the-blocks/features/blocks.py
@@ -5,15 +5,16 @@ import random
 block_width = 40
 block_height = 40
 block_speed = 200
-spawn_interval = 1.2
+spawn_interval = 0.6
 
 blocks = []
 spawn_timer = 0
+difficulty_timer = 0
 
 
 def update_blocks(dt):
     """Laat blokken vallen en voeg nieuwe toe wanneer nodig."""
-    global spawn_timer
+    global spawn_timer, difficulty_timer, block_speed, spawn_interval
 
     # Nieuwe blok toevoegen
     spawn_timer += dt
@@ -21,6 +22,14 @@ def update_blocks(dt):
         spawn_timer = 0
         x = random.randint(0, 800 - block_width)
         blocks.append([x, -block_height])  # Start net boven het scherm
+
+    # Moeilijkheid verhogen
+    difficulty_timer += dt
+    if difficulty_timer >= 3:
+        difficulty_timer = 0
+        block_speed += 50
+        if spawn_interval < 0.4:
+            spawn_interval = 0.4
 
     # Blokken naar beneden laten bewegen
     for block in blocks:
@@ -49,4 +58,9 @@ def check_collision(player_x, player_y, player_width, player_height):
 
 
 def reset_blocks():
+    global blocks, block_speed, spawn_timer, spawn_interval, difficulty_timer
     blocks.clear()
+    block_speed = 200
+    spawn_interval = 1.2
+    spawn_timer = 0
+    difficulty_timer = 0
