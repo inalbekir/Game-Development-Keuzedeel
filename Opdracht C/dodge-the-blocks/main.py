b0c031778c5a2396a733965d5daea30d39feada7
Bekir Inal <bekirinal5415@gmail.com>
2025-04-24 12:07:11 +0200

    Game Over Scherm

--- Opdracht C/dodge-the-blocks/main.py
+++ Opdracht C/dodge-the-blocks/main.py
@@ -2,12 +2,14 @@ from raylibpy import *
 from features.player import *
 from features.blocks import *
 
+game_over = False
 lives = 1
 
 init_window(800, 600, b"Dodge the Blocks")
 set_target_fps(60)
 
-while not window_should_close():
+while not window_should_close() and not game_over:
+
     dt = get_frame_time()
 
     update_player(dt)
@@ -15,7 +17,7 @@ while not window_should_close():
 
     if check_collision(*get_player_rect()):
         print("BOTSING! Game Over.")
-        break
+        game_over = True
 
     begin_drawing()
     clear_background(RAYWHITE)
@@ -24,6 +26,17 @@ while not window_should_close():
     draw_blocks()
 
     end_drawing()
+
+# Game over scherm
+while not window_should_close():
+    begin_drawing()
+    clear_background(BLACK)
+    draw_text("GAME OVER", 290, 250, 40, RED)
+    draw_text("Druk op ESC om af te sluiten", 260, 310, 20, WHITE)
+    end_drawing()
+
+    if is_key_down(KEY_ESCAPE):  # beter dan key_pressed
+        break
 
 
 close_window()
