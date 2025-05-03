777881d0b243a32fa835780d70ab01489600b608
Bekir Inal <bekirinal5415@gmail.com>
2025-05-03 04:32:25 +0200

    Branding toegevoegd

---
+++ Opdracht C/dodge-the-blocks/background.py
@@ -0,0 +1,73 @@
+from raylibpy import *
+from features.player import player_texture
+from features.blocks import block_texture
+from features.powerup import powerup_texture
+
+background_texture = None
+logo_texture = None
+
+
+def load_background_texture():
+    global background_texture
+    background_texture = load_texture("assets/images/background.png")
+
+
+def draw_background():
+    if background_texture:
+        screen_width = get_screen_width()
+        screen_height = get_screen_height()
+
+        # Orijinal sprite boyutları
+        texture_width = background_texture.width
+        texture_height = background_texture.height
+
+        # Kaynak dikdörtgen (orijinal resmin tamamı)
+        source = Rectangle(0, 0, texture_width, texture_height)
+
+        # Hedef dikdörtgen (ekranı tam kaplayacak)
+        dest = Rectangle(0, 0, screen_width, screen_height)
+
+        draw_texture_pro(
+            background_texture,
+            source,
+            dest,
+            Vector2(0, 0),   # Origin = köşe
+            0.0,             # Rotation
+            WHITE
+        )
+
+
+
+def show_start_screen():
+
+    while not window_should_close():
+        begin_drawing()
+        clear_background(RAYWHITE)
+        draw_background()
+        draw_logo()
+
+        draw_text("DODGE THE BLOCKS", 185, 250, 40, RED)
+        draw_text("Druk op ENTER om te beginnen", 10, 10, 20, BLACK)
+
+        end_drawing()
+
+        if is_key_pressed(KEY_ENTER):
+            return True
+        if is_key_pressed(KEY_ESCAPE):
+            return False
+
+
+
+
+
+def draw_logo():
+    global logo_texture
+    if not logo_texture:
+        logo_texture = load_texture("assets/images/logo.png")
+
+    # Logoyu çiz
+    draw_texture_ex(logo_texture, Vector2(515, 180), 0.0, 3, WHITE)
+
+    # Yanına yazı çiz (slogan gibi)
+    draw_text("BEST EDUCATION", 220, 190, 30, DARKBLUE)
+    draw_text("Wij lanceren je de toekomst in!", 170, 320, 30, GOLD)
--- Opdracht C/dodge-the-blocks/main.py
+++ Opdracht C/dodge-the-blocks/main.py
@@ -1,14 +1,18 @@
 from features.player import *
 from features.blocks import *
 from features.powerup import *
-from features.background import *
+from background import *
 from features.sounds import *
 
 
 init_window(800, 600, b"Dodge the Blocks")
 set_target_fps(60)
-
 init_audio_device()
+load_background_texture()
+
+if not show_start_screen():
+    close_window()
+    exit()
 
 
 def run_game():
@@ -22,11 +26,9 @@ def run_game():
     score_timer = 0
 
     load_sounds()
-
     load_player_texture()
     load_block_texture()
     load_powerup_texture()
-    load_background_texture()
 
     while not window_should_close() and not game_over:
 
@@ -42,7 +44,7 @@ def run_game():
             game_over = True
             play_explosion_sound()
             play_lose_sound()
-            return "lose"  #  burası onemli
+            return "lose"  #  burası onemli unutma
 
 
         if check_powerup_collision(*get_player_rect()):
@@ -55,11 +57,11 @@ def run_game():
             score += 1
             score_timer = 0
 
-            if score >= 3:
+            if score >= 10:
                 print("YOU WIN!")
                 game_won = True
                 play_win_sound()
-                return "win"  # <-- burası önemli
+                return "win"
 
         begin_drawing()
         clear_background(RAYWHITE)
@@ -83,9 +85,10 @@ while True:
         while not window_should_close():
             begin_drawing()
             draw_background()
+            draw_logo()
             draw_player_win()
-            draw_text("YOU WIN", 220, 250, 80, GREEN)
-            draw_text("'ESC' om af te sluiten  \n'R' om opnieuw te beginnen", 10, 10, 20, BLACK)
+            draw_text("YOU WIN", 220, 230, 80, GREEN)
+            draw_text("Druk op ESC om af te sluiten  \nDruk op R om opnieuw te beginnen", 10, 10, 20, BLACK)
             end_drawing()
 
             if is_key_down(KEY_ESCAPE):
@@ -103,9 +106,10 @@ while True:
         while not window_should_close():
             begin_drawing()
             draw_background()
+            draw_logo()
             draw_player_dead()
-            draw_text("GAME OVER", 160, 250, 80, RED)
-            draw_text("'ESC' om af te sluiten  \n'R' om opnieuw te beginnen", 10, 10, 20, BLACK)
+            draw_text("GAME OVER", 160, 230, 80, RED)
+            draw_text("Druk op ESC om af te sluiten  \nDruk op R om opnieuw te beginnen", 10, 10, 20, BLACK)
             end_drawing()
 
             if is_key_down(KEY_ESCAPE):
